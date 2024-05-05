8f2a652894791a05f5c45fa65fff9dfea6e8e295
aLearningLad <mocmanca@gmail.com>
2024-05-05 18:43:35 +0200

    further changes

--- logic.js
+++ logic.js
@@ -1,5 +1,12 @@
 const gameBoard = document.querySelector(".game");
 const scoreContainer = document.querySelector(".scoreContainer");
+let randomIndex;
+const currentScore = document.querySelector(".currentScore");
+let liveScore = 0;
+let timer;
+const clock = document.querySelector(".clock");
+let playing = true;
+let time = 25;
 
 const squareCount = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -10,16 +17,54 @@ for (value of squareCount) {
   gameBoard.appendChild(square);
 }
 
+const allSquares = document.querySelectorAll(".square");
+
 function moleMovement() {
-  const allSquares = document.querySelectorAll(".square");
   allSquares.forEach((square) => {
     square.classList.remove("mole");
   });
 
-  let randomIndex = Math.floor(Math.random() * 10);
+  randomIndex = Math.floor(Math.random() * 10);
   let randomSquare = allSquares[randomIndex];
 
   randomSquare.classList.add("mole");
 }
 
-setInterval(moleMovement, 800);
+function smash(id) {
+  if (id === allSquares[randomIndex].id) {
+    liveScore += 1;
+    currentScore.innerHTML = `Your current score is ${liveScore}`;
+  } else {
+    if (liveScore > 0) {
+      liveScore -= 1;
+      currentScore.innerHTML = `Your current score is ${liveScore}`;
+
+      return;
+    }
+    liveScore = 0;
+    currentScore.innerHTML = `Your current score is ${liveScore}`;
+    alert(`Pick your game up! You're already at zero!`);
+  }
+}
+
+allSquares.forEach((square) =>
+  square.addEventListener("mousedown", () => {
+    smash(square.id);
+  })
+);
+
+const intervalId = setInterval(moleMovement, 1500);
+
+const masterIntervalId = setInterval(() => {
+  if (time > 0) {
+    time -= 1;
+    clock.innerHTML = `Time remaining: ${time}`;
+  } else if (time === 0) {
+    alert(`Game's up! You scored ${liveScore}`);
+    clearInterval(masterIntervalId);
+    clearInterval(intervalId);
+    clock.innerHTML = "Time's up! Reload page to play again";
+  }
+}, 1000);
+
+// ADD LEVELS ====>>> LEVEL INCREASES REDUCE AMOUNT OF TIME, AND MOLE FLASHES QUICKER AND QUICKER
